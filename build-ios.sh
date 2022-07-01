e0b09b5a6b659b1a29c88b70f5a66e71a255c633
LEE <15267030696@163.com>
2022-07-01 19:25:53 +0800

    1.更新；

--- build-ios.sh
+++ build-ios.sh
@@ -1,2 +1,4 @@
-cmake -DCMAKE_TOOLCHAIN_FILE=./ios.cmake -DIOS_PLATFORM=OS -H. -Bbuild.ios -GXcode
+rm -rf build.ios
+cmake -DCMAKE_TOOLCHAIN_FILE=./ios.cmake -DIOS_PLATFORM=OS -H. -Bbuild.ios
 cmake --build build.ios/ --config Release
+make

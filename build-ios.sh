20c01aa09b13d97eb0ddb05fbcc5a4157e394de5
LEE <15267030696@163.com>
2022-07-04 15:43:46 +0800

    1.更新

--- build-ios.sh
+++ build-ios.sh
@@ -1,4 +1,5 @@
-rm -rf build.ios
-cmake -DCMAKE_TOOLCHAIN_FILE=./ios.cmake -DIOS_PLATFORM=OS -H. -Bbuild.ios --debug-find
+BUILD_DIR=build.ios
+rm -rf ${BUILD_DIR}
+cmake -DCMAKE_TOOLCHAIN_FILE=./ios.cmake -DIOS_PLATFORM=OS -H. -B ${BUILD_DIR}
 cmake --build build.ios/ --config Release
 make
